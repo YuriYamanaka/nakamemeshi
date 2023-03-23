110282f9d4b67dcae982e674e034559d0e943e9b
githubYuriYamanaka <github126098998+YuriYamanaka@users.noreply.github.com>
2023-03-23 22:51:58 +0900

    第一回変更

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,48 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]  
+  def index
+        @foods = Food.all
+    end
+
+    def new
+        @foods = Food.new
+      end
+    
+      def create
+        food = Food.new(food_params)
+        food.user_id = current_user.id
+        if food.save!
+          redirect_to :action => "index"
+        else
+          redirect_to :action => "new"
+        end
+      end
+
+      def show
+        @food = Food.find(params[:id])
+      end
+
+      def edit
+        @food = Food.find(params[:id])
+      end
+
+      def update
+        food = Food.find(params[:id])
+        if food.update(food_params)
+          redirect_to :action => "show", :id => food.id
+        else
+          redirect_to :action => "new"
+        end
+      end  
+
+      def destroy
+        food = Food.find(params[:id])
+        food.destroy
+        redirect_to action: :index
+      end
+
+      private
+      def food_params
+        params.require(:food).permit(:name, :genre, :address, :cost, :about, :date)
+      end
 end
