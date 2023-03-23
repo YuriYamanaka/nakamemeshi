a70513b25a54f0b9d2e3c6f9b755e68c9b5392a7
githubYuriYamanaka <github126098998+YuriYamanaka@users.noreply.github.com>
2023-03-24 00:43:45 +0900

    うまいごはん

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  
+
   resources :foods do
     resources :likes, only: [:create, :destroy]
   end
